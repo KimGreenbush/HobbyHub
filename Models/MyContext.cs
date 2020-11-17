a4c495f3f9833139b932506bc4c7701ac1e7bbca
Kim G <Kim_greenbush@icloud.com>
2020-11-17 11:04:37 -0800

    connecting dbcontext

--- Models/MyContext.cs
+++ Models/MyContext.cs
@@ -4,7 +4,7 @@ namespace Csharp_belt.Models
 {
     public class MyContext : DbContext
     {
-        public MyContext(DbContextOptions options) : base(options) { }
+        public MyContext (DbContextOptions options) : base(options) { }
 
         public DbSet<User> Users { get; set; }
         public DbSet<Hobby> Hobbies { get; set; }
